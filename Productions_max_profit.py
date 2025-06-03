5729e828b3d2cc311433e39acfb55b87ace13d19
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-03 23:01:26 +0330

    creat base var of func

--- Productions_max_profit.py
+++ Productions_max_profit.py
@@ -2,3 +2,7 @@ def solve_product(p, l, m, L, M):
     p1, p2, p3 = p
     l1, l2, l3 = l
     m1, m2, m3 = m
+
+    best_profit = -float('inf')
+    best_sol = (0.0, 0, 0)
+    max_B = max(L // l2, M // m2)
