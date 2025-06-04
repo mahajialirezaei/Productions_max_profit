f058c2a78110ca8345dc265edf5a93d0e1bbe4ae
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-04 14:17:47 +0330

    create func find_upper_bound_b

--- Productions_max_profit.py
+++ Productions_max_profit.py
@@ -3,6 +3,33 @@ def print_result_func(best_sol, best_profit):
     print("total: " + str(best_profit))
 
 
+def find_upper_bound_b(l1, m1, remaining_l_b, remaining_m_b, l3, m3, p1, p3, p2):
+    if l1 > 0 and m1 > 0:
+        x1_case1 = min(remaining_l_b / l1, remaining_m_b / m1)
+        case1 = p1 * x1_case1
+    else:
+        case1 = 0.0
+
+    if l3 > 0 and m3 > 0:
+        x3_case2 = min(remaining_l_b / l3, remaining_m_b / m3)
+        case2 = p3 * x3_case2
+    else:
+        case2 = 0.0
+
+    det = l1 * m3 - l3 * m1
+    if det != 0:
+        x1_case3 = (remaining_l_b * m3 - remaining_m_b * l3) / det
+        x3_case3 = (-1 * remaining_l_b * m1 + remaining_m_b * l1) / det
+        if x1_case3 >= 0 and x3_case3 >= 0:
+            case3 = p1 * x1_case3 + p3 * x3_case3
+        else:
+            case3 = 0.0
+    else:
+        case3 = 0.0
+
+    bound_b = p2 + max(case1, case2, case3)
+    return bound_b
+
 def solve_product(p, l, m, L, M):
     p1, p2, p3 = p
     l1, l2, l3 = l
@@ -20,31 +47,7 @@ def solve_product(p, l, m, L, M):
         if remaining_l_b < 0 or remaining_m_b < 0:
             continue
 
-        if l1 > 0 and m1 > 0:
-            x1_case1 = min(remaining_l_b / l1, remaining_m_b / m1)
-            case1 = p1 * x1_case1
-        else:
-            case1 = 0.0
-
-        if l3 > 0 and m3 > 0:
-            x3_case2 = min(remaining_l_b / l3, remaining_m_b / m3)
-            case2 = p3 * x3_case2
-        else:
-            case2 = 0.0
-
-
-        det = l1 * m3 - l3 * m1
-        if det != 0:
-            x1_case3 = (remaining_l_b * m3 - remaining_m_b * l3) / det
-            x3_case3 = (-1 * remaining_l_b * m1 + remaining_m_b * l1) / det
-            if x1_case3 >=0 and x3_case3 >= 0:
-                case3 = p1 * x1_case3 + p3 * x3_case3
-            else:
-                case3 = 0.0
-        else:
-            case3 = 0.0
-
-        bound_b = p2 + max(case1, case2, case3)
+        bound_b = find_upper_bound_b(l1, m1, remaining_l_b, remaining_m_b, l3, m3, p1, p3, p2)
         if bound_b <= best_profit:
             continue
         max_C = min(remaining_l_b // l3 if l3 > 0 else 0,
